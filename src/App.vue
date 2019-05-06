bbf1e13ad20ee02bd7b070c6ffeb7500d3ad6afc
keita <ba0503naky@gmail.com>
2019-05-06 15:55:01 +0900

    video test

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,42 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column">
+      <button class="button" @click="cameraStart">カメラスタート</button>
+    </div>
+    <video autoplay></video>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  methods: {
+    cameraStart() {
+      const p = navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          width: this.width,
+          height: this.height,
+          frameRate: { ideal: 5, max: 15 }
+        }
+      });
+      p.then(function(mediaStream) {
+        document.querySelector("video").srcObject = mediaStream;
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
